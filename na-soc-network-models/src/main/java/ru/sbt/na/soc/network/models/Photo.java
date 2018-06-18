a86efb4c36e43939a62ad422d2f5b08ae8ddbe06
Sherbakov_A_AS <Smersh.Root@gmail.com>
2018-06-18 19:00:21 +0500

    Описание к предыдущему коммиту: Реализовал модуль Сообщений. Рефакторинг методов Профиля.

--- na-soc-network-models/src/main/java/ru/sbt/na/soc/network/models/Photo.java
+++ na-soc-network-models/src/main/java/ru/sbt/na/soc/network/models/Photo.java
@@ -1,5 +1,24 @@
 package ru.sbt.na.soc.network.models;
 
 public class Photo {
+    private Long id;
+    private String src;
 
+    public Photo() {
+    }
+
+    public Photo(Long id) {
+        this.id=id;
+    }
+    public Photo(String src) {
+        this.src=src;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
---
+++ na-soc-network-search-add-friend/src/main/java/sbt/na/soc/network/update/frendlist/updateFrendList.java
@@ -0,0 +1,29 @@
+package sbt.na.soc.network.update.frendlist;
+
+import sbt.na.soc.network.profile.ProfileBasic;
+import sbt.na.soc.network.communication.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface updateFrendList {
+
+    String checkAdd(ProfileBasic profileInitiator, ProfileBasic profile);
+
+    boolean add (ProfileBasic profileInitiator, ProfileBasic profile);
+
+    void remove (ProfileBasic profileInitiator, ProfileBasic profile);
+
+    default List<String> add (ProfileBasic profileInitiator, List<ProfileBasic> profiles){
+        List<String> messages=new ArrayList<>();
+        for(ProfileBasic profile:profiles){
+            messages.add(checkAdd (profileInitiator, profile));
+            add (profileInitiator, profile);
+        }
+        return messages;
+    }
+
+    void remove (ProfileBasic profileInitiator, List<ProfileBasic> profiles);
+
+    boolean add (ProfileBasic profileInitiator, ProfileBasic profile, Message message);
+}
